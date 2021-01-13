e5e57b3baf0635ca0d5d84fdc87f6e58581ecd9e
Lakshay saini <lakshaysaini2013@gmail.com>
2021-01-13 23:13:45 +0530

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Navbar = () => {
   const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
+  const [auth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
